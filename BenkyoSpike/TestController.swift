6ec8db28766b2307d4e9b9f911f53cef98af0018
liyicky <liyicky@gmail.com>
2017-03-28 20:45:44 +0900

    Spiked Mini Scrollview

--- BenkyoSpike/TestController.swift
+++ BenkyoSpike/TestController.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2017 Liyicky. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TestController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView?
+    @IBOutlet weak var pageControl: UIPageControl?
+    var button1: UIButton!
+    var button2: UIButton!
+    var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let screenWidth: CGFloat = (self.scrollView?.bounds.size.width)!
+        let screenHeight: CGFloat = (self.scrollView?.bounds.size.height)!
+        
+        self.scrollView?.contentSize = CGSize(width: screenWidth * 4, height: (self.scrollView?.bounds.size.height)!)
+        
+        
+        self.button1 = UIButton(frame: CGRect(x: screenWidth/2, y: screenHeight/2, width: 100, height: 50))
+        button1.backgroundColor = .green
+        button1.setTitle("Test Button 1", for: .normal)
+        
+        self.button2 = UIButton(frame: CGRect(x: screenWidth*2, y: screenHeight/2, width: 100, height: 50))
+        button2.backgroundColor = .green
+        button2.setTitle("Test Button 2", for: .normal)
+        
+        self.button3 = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button3.backgroundColor = .green
+        button3.setTitle("Test Button 3", for: .normal)
+        
+        scrollView?.addSubview(button1)
+        scrollView?.addSubview(button2)
+        scrollView?.addSubview(button3)
+    }
+    
+}
